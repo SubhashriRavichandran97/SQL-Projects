d83bc891577206c019d742040c222069585058a7
SubhashriRavichandran97 <subhashri.ravi19@gmail.com>
2025-03-12 19:28:15 +0100

    Update ddl_bronze.sql

--- DataWarehouse_and_Analytics/scripts/bronze/ddl_bronze.sql
+++ DataWarehouse_and_Analytics/scripts/bronze/ddl_bronze.sql
@@ -1,8 +1,7 @@
--- DDL Script to create Bronze tables
--- Drop tables if they exist and create new ones
-
-DROP TABLE IF EXISTS bronze.crm_cust_info;
-CREATE TABLE bronze.crm_cust_info (
+-- Create tables in bronze schema
+USE bronze;
+DROP TABLE IF EXISTS crm_cust_info;
+CREATE TABLE crm_cust_info (
     cst_id              INT,
     cst_key             VARCHAR(50),
     cst_firstname       VARCHAR(50),
@@ -12,8 +11,8 @@ CREATE TABLE bronze.crm_cust_info (
     cst_create_date     DATE
 );
 
-DROP TABLE IF EXISTS bronze.crm_prd_info;
-CREATE TABLE bronze.crm_prd_info (
+DROP TABLE IF EXISTS crm_prd_info;
+CREATE TABLE crm_prd_info (
     prd_id       INT,
     prd_key      VARCHAR(50),
     prd_nm       VARCHAR(50),
@@ -23,37 +22,36 @@ CREATE TABLE bronze.crm_prd_info (
     prd_end_dt   DATETIME
 );
 
-DROP TABLE IF EXISTS bronze.crm_sales_details;
-CREATE TABLE bronze.crm_sales_details (
+DROP TABLE IF EXISTS crm_sales_details;
+CREATE TABLE crm_sales_details (
     sls_ord_num  VARCHAR(50),
     sls_prd_key  VARCHAR(50),
     sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
+    sls_order_dt DATE,
+    sls_ship_dt  DATE,
+    sls_due_dt   DATE,
     sls_sales    INT,
     sls_quantity INT,
     sls_price    INT
 );
 
-DROP TABLE IF EXISTS bronze.erp_loc_a101;
-CREATE TABLE bronze.erp_loc_a101 (
+DROP TABLE IF EXISTS erp_loc_a101;
+CREATE TABLE erp_loc_a101 (
     cid    VARCHAR(50),
     cntry  VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS bronze.erp_cust_az12;
-CREATE TABLE bronze.erp_cust_az12 (
+DROP TABLE IF EXISTS erp_cust_az12;
+CREATE TABLE erp_cust_az12 (
     cid    VARCHAR(50),
     bdate  DATE,
     gen    VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
-CREATE TABLE bronze.erp_px_cat_g1v2 (
+DROP TABLE IF EXISTS erp_px_cat_g1v2;
+CREATE TABLE erp_px_cat_g1v2 (
     id           VARCHAR(50),
     cat          VARCHAR(50),
     subcat       VARCHAR(50),
     maintenance  VARCHAR(50)
 );
-
