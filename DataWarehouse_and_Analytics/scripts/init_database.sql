e117f1834ffa33fb323d7c8a3c8f717af0761076
SubhashriRavichandran97 <subhashri.ravi19@gmail.com>
2025-03-12 16:07:55 +0100

    Create init_database.sql

---
+++ DataWarehouse_and_Analytics/scripts/init_database.sql
@@ -0,0 +1,22 @@
+=============================================================
+Create Database and Schemas
+=============================================================
+USE master;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create the Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
